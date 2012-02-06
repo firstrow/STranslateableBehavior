3aa981071a9de94d08e7f8e7f98747e512962346
Fredrik Wollsén <fredrik@neam.se>
2012-02-06 10:52:12 +0200

    More accurate model-detection
    Based on FullCrudGenerator's getModels logic from gii-template-collection

--- protected/commands/TranslateCommand.php
+++ protected/commands/TranslateCommand.php
@@ -8,7 +8,6 @@
 class TranslateCommand extends CConsoleCommand 
 {
 
-	public $search_str ='application.extensions.STranslateableBehavior.STranslateableBehavior';
 	public $models = array();
 	public $langs = null;
 	public $up = array();
@@ -21,21 +20,8 @@ class TranslateCommand extends CConsoleCommand
 	{
 		$this->_loadLanguages();
 
-		$path = realpath(dirname(__FILE__).'/../');
-		$files = CFileHelper::findFiles($path, array('fileTypes'=>array('php')));
-		
-		if (!sizeof($files)) return false;
-		
-		// Try to find all models with translate behavior enabled.
-		foreach ($files as $file)
-		{
-			$content = file_get_contents($file);
-			if (strpos($content, $this->search_str) && basename($file) != 'TranslateCommand.php')
-			{
-				$this->_loadModelFile($file);
-			}
-		} 
-	
+		$this->models = $this->getModels();
+
 		if (sizeof($this->models) > 0)
 		{
 			$this->_createMigration();
@@ -155,4 +141,65 @@ $phpCode = '<?php
 		print "To apply migration enter: ./yiic migrate\n";
 	}
 
+	// Originally from gii-template-collection / fullCrud / FullCrudGenerator.php
+	protected function getModels() {
+		$models = array();
+		$aliases = array();
+		$aliases[] = 'application.models';
+		foreach (Yii::app()->getModules() as $moduleName => $config) {
+			if($moduleName != 'gii')
+				$aliases[] = $moduleName . ".models";
+		}
+
+		foreach ($aliases as $alias) {
+			if (!is_dir(Yii::getPathOfAlias($alias))) continue;
+			$files = scandir(Yii::getPathOfAlias($alias));
+			Yii::import($alias.".*");
+			foreach ($files as $file) {
+				if ($fileClassName = $this->checkFile($file, $alias)) {
+						$classname = sprintf('%s.%s',$alias,$fileClassName);
+						Yii::import($classname);
+					try {
+						$model = @new $fileClassName;
+						if (method_exists($model, 'translate')) {
+							if (method_exists($model, 'behaviors')) {
+								$behaviors = $model->behaviors();
+								if (isset($behaviors['translate']) && strpos($behaviors['translate']['class'], 'STranslateableBehavior') !== false) {
+									$models[$classname] = $model;
+								}
+							}
+						}
+					} catch (ErrorException $e) {
+						break;
+					} catch (CDbException $e) {
+						break;
+					} catch (Exception $e) {
+						break;
+					}
+				}
+			}
+		}
+
+		return $models;
+	}
+
+	// Imported from gii-template-collection / fullCrud / FullCrudGenerator.php
+	private function checkFile($file, $alias = '') {
+		if (substr($file, 0, 1) !== '.'
+				&& substr($file, 0, 2) !== '..'
+				&& substr($file, 0, 4) !== 'Base'
+			&& $file != 'GActiveRecord'
+			&& strtolower(substr($file, -4)) === '.php') {
+			$fileClassName = substr($file, 0, strpos($file, '.'));
+			if (class_exists($fileClassName)
+					&& is_subclass_of($fileClassName, 'CActiveRecord')) {
+				$fileClass = new ReflectionClass($fileClassName);
+				if ($fileClass->isAbstract())
+					return null;
+				else
+					return $models[] = $fileClassName;
+			}
+		}
+	}
+	
 }
